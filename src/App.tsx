2be1dbefc77e6663c38c4410c82d8f2eff948515
Sachin Kumar <lucky002954@gmail.com>
2025-09-30 15:40:19 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ function App() {
   return (
     <div className="relative overflow-x-show">
       {/* Screen Transition */}
-      <div className={`transition-all duration-200 ease-in-out ${
+      <div className={`transition-all duration-100 ease-in-out ${
         currentScreen === 'welcome' ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-full absolute inset-0 pointer-events-none'
       }`}>
         {currentScreen === 'welcome' && (
